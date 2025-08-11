51f29c78ee374cec9b0f038396ded87dac2d43ee
iancorbett <iancorbett1324@gmail.com>
2025-08-11 13:20:49 -0700

    add bottom border to navbar

--- kylePortfolio/src/components/Navbar.jsx
+++ kylePortfolio/src/components/Navbar.jsx
@@ -25,7 +25,7 @@ export const Navbar = () => {
 return (
     <nav 
     className={cn(
-        "fixed w-full z-40 transition-all duration-300", 
+        "fixed w-full z-40 transition-all duration-300 border-b", 
         isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
     )}
     >
@@ -80,6 +80,7 @@ return (
             </div>
         </div>
         </div>
+        
     </nav>
     );
 }
