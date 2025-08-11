1b59178c148c5862e133918e17f6bc7b92678dde
iancorbett <iancorbett1324@gmail.com>
2025-08-11 13:24:48 -0700

    create about jsx

--- kylePortfolio/src/components/Footer.jsx
+++ kylePortfolio/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { ArrowUp } from "lucide-react"
 
 export const Footer = () => {
     return (
-        <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8flex flex-wrap justify-between items-center">
+        <footer className="py-12 px-4 bg-card relative border-t  mt-12 pt-8flex flex-wrap justify-between items-center">
             <p className="text-sm text-muted-foreground">
                 &copy; {new Date().getFullYear()} Kyle Corbett Portfolio
             </p>
--- kylePortfolio/src/pages/Home.jsx
+++ kylePortfolio/src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import { Footer } from "../components/Footer";
 import { Navbar } from "../components/Navbar";
 
 export const Home = () => {
     return (
     <div>
         <Navbar />
+       
     </div>
     );
 }
