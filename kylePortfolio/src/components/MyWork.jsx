fc6fb9b8c2d97fb23c5ae3e981f237029f868919
iancorbett <iancorbett1324@gmail.com>
2025-08-12 10:33:11 -0700

    update styling for mywork section

--- kylePortfolio/src/components/MyWork.jsx
+++ kylePortfolio/src/components/MyWork.jsx
@@ -1,4 +1,4 @@
-import { Github, ExternalLink } from "lucide-react";
+import { Github, ExternalLink, ArrowBigDown } from "lucide-react";
 import { cn } from "../lib/utils";
 import screenshot from "../assets/ProjectPic.png"; 
 import Project1 from "@/assets/Project1.png";
@@ -128,6 +128,48 @@ export default function MyWorkSection() {
           </div>
         </article>
       </div>
+      <p className="text-center mb-6 max-w-2xl mx-auto text-xl mt-18">
+       These are a few other projects that I have built 
+        </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {projects.map((project, key) => (
+                <div key={key} 
+                className="group bg-card  overflow-hidden rounded-lg shadow-xs card-hover"
+                >
+                    <div>
+                        <img src={project.image} alt={project.title} className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-110"/>
+                    </div>
+
+                    <div className="p-6">
+
+                        <div className="flex flex-wrap gap-2 mb-4">
+                            {project.tags.map((tag, key) => (
+                                
+                                <span 
+                                key={key}
+                                className="px-2 py-1 text-xs border font-medium rounded-full bg-secondary text-secondary-foreground">
+                                    {tag}
+                                </span>
+                            ))}
+                        </div>
+
+                    
+                        <h3 className="text-xl font-semibold mb-1">{project.title}</h3>
+                        <p className="text-sm mb-4">{project.description}</p>
+                        <div className="flex justify-between items-center">
+                            <div className="flex space-x-3">
+                               
+                                <a href={project.githubUrl} 
+                                target="_blank"
+                                >
+                                    <Github size={20}/>Visit Repo
+                                </a>
+                            </div>
+                            </div>
+                        </div>
+                </div>
+            ))}
+        </div>
     </section>
   );
 }
